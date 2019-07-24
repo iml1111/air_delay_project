f27d29346ca69f55ecb98c34c882e742402c8bb6
iml1111 <shin10256@gmail.com>
2019-07-24 17:59:46 +0900

    error fix

--- src/learning.py
+++ src/learning.py
@@ -24,11 +24,11 @@ def proc(df):
 	    df1 = df_x.iloc[df_index]
 	    val1 = df_x.iloc[val_index]
 	#학습데이터셋 스플릿하기
-	df_y = df_x['DLY']
-	df_x = df_x.drop(['DLY'], axis=1)
+	df_y = df1['DLY']
+	df_x = df1.drop(['DLY'], axis=1)
 	#밸리드데이터셋 스플릿하기
-	val_y = val_x['DLY']
-	val_x = val_x.drop(['DLY'], axis=1)
+	val_y = val1['DLY']
+	val_x = val1.drop(['DLY'], axis=1)
 
 	###### 학습하기
 	models = [
